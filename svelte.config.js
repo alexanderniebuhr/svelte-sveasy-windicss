c389da668456e2a618783ed656671b4e035bb628
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-04-02 19:41:31 +0200

    chore: :fire: remove deprecated

--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preprocess: require("svelte-windicss-preprocess").preprocess({
-    silent: false,
-    debug: true,
-    compile: true,
-    config: "tailwind.config.js",
-    prefix: "windi-",
-  }),
-};
